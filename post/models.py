36f8f639999f333b87132046cfc64409c7df4f10
yasersarhan <sarhan.whitewhale@gmail.com>
2021-01-19 21:12:14 +0200

    create function add new post

--- post/models.py
+++ post/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 CATEGORY_CHOICES = (
     ('WB','Web Development'),
     ('DB','Desktop Development'), 
@@ -8,7 +9,7 @@ CATEGORY_CHOICES = (
 class Post(models.Model):
     title = models.CharField(max_length=50, unique=True)
     content = models.TextField(max_length=1000)
-    created_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
     published = models.BooleanField(null=False, default=True)
     email = models.EmailField(max_length=254, null=True, blank=True, default='Test@')
     views_count = models.PositiveIntegerField(default=0, null=True)
--- post/views.py
+++ post/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
 # Create your views here.
+
 
 def post_list(request):
     all_posts = Post.objects.all()
-    print(all_posts)
-    return render(request,'all_posts.html',{'all_posts':all_posts})
-
-
+    return render(request,'post/post_list.html',{'all_posts':all_posts}) 
 
 def post_detail(request,id):
     post = Post.objects.get(id=id)
-    return render(request,'post.html',{'post':post})
+    return render(request,'post/post_detail.html',{'post':post})
 
+
+def post_create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = PostForm()
+
+    return render(request,'post/post_create.html',{'form':form })
+
+
+def post_edit(request):
+    pass
+     
